90c45b57de2985d3228b2ccf7d22d6743dca5b1b
wingmeng <wingmeng@qq.com>
2019-02-26 09:26:53 +0800

    fix: 多个作者时名称混在一起的bug

--- src/booksItem.js
+++ src/booksItem.js
@@ -69,7 +69,9 @@ class BooksItem extends Component {
                 </div>
               </div>
               <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors}</div>
+              <div className="book-authors">
+                {book.authors ? book.authors.join(', ') : ''}
+              </div>
             </div>
           </li>
         ))}
